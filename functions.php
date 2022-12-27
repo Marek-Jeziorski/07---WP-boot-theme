48de69195d618ae241aba7ab3b7ec1020ad6eb7e
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-12-27 15:30:56 +0100

    @wordpress/scripts
    @wordpress/scripts bundler zainstalowany
    to będzie na razie koniec tego THEME
    zakładam 08---WP-boot-theme
    baza i wszystkie ustawienia WP zostają
    zacznę importować statyczny model  z 06---bootClean-wordpres

--- functions.php
+++ functions.php
@@ -3,13 +3,13 @@
 // Load Stylesheets
 function load_css()
 {
-    wp_register_style('variables', get_template_directory_uri() . '/assets/css/variables.css', array(), false);
+    wp_register_style('variables', get_template_directory_uri() . '/src/css/variables.css', array(), false);
     wp_enqueue_style('variables');
 
-    wp_register_style('global', get_template_directory_uri() . '/assets/css/global.css', array(), false);
+    wp_register_style('global', get_template_directory_uri() . '/src/css/global.css', array(), false);
     wp_enqueue_style('global');
 
-    wp_register_style('menu', get_template_directory_uri() . '/assets/css/menu.css', array(), false);
+    wp_register_style('menu', get_template_directory_uri() . '/src/css/menu.css', array(), false);
     wp_enqueue_style('menu');
 
 }
@@ -19,8 +19,8 @@ add_action('wp_enqueue_scripts', 'load_css');
 // Load JavaScript
 function load_js()
 {
-    wp_register_script('main-js', get_template_directory_uri() . '/assets/js/main.js', array(), false, true);
-    wp_enqueue_script('main-js');
+    wp_register_script('index.js', get_template_directory_uri() . '/src/index.js', array(), false, true);
+    wp_enqueue_script('index.js');
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
